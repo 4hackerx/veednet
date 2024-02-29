e6139d6b71b882a6bcac7a625edcdd4feb64234d
solomon8392 <solomonisaac8392@gmail.com>
2024-02-29 14:42:14 +0100

    scroll animation for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import Image from "next/image";
-import Navbar from "./Navbar/page";
-import Homee from "./Home/page";
+// import Image from "next/image";
+import Navbar from "@/library/components/organisms/Navbar";
+import VideoSelector from "@/library/components/molecules/VideoSelector";
+import CollapsibleDemo from "@/library/components/molecules/CompresorColabsible";
+import { VideoCompressor } from "@/library/components/services/video-compressor/page";
+
+
+
+
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Homee />
+      <VideoSelector/>
+      <CollapsibleDemo/>
+      <VideoCompressor />
     </div>
     
   );
