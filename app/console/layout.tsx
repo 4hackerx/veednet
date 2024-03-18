bf3324b5e60b576be0823ca96c1ad2d35f09fcf4
kelvinpraises <kelvinpraises@gmail.com>
2024-03-18 16:18:27 +0100

    chore: remove unused imports

--- app/console/layout.tsx
+++ app/console/layout.tsx
@@ -1,10 +1,3 @@
-import Navbar from "@/library/components/organisms/Navbar";
-import { cn } from "@/library/utils";
-import "@/library/styles/globals.css";
-
-import { Inter, Outfit } from "next/font/google";
-import localFont from "next/font/local";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -12,7 +5,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex w-full flex-col items-center gap-8 pt-8 h-screen ">
-      <img src="logo-vertical.svg" alt="logo" />
+      <img src="/logo-vertical.svg" alt="logo" />
       {children}
     </div>
   );
