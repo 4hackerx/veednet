ceafb401a4591b8ae0a82b4c983d8ad382ccb23f
kelvinpraises <kelvinpraises@gmail.com>
2024-03-18 16:21:06 +0100

    feat: add slash to image path

--- library/components/organisms/Navbar.tsx
+++ library/components/organisms/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-6 h-[80px] z-[99999]">
-      <Image src={"logo.svg"} alt={"logo"} width={"150"} height={"31"} />
+      <Image src={"/logo.svg"} alt={"logo"} width={"150"} height={"31"} />
       <button className=" font-outfit font-[16px] bg-black text-[white] px-6 rounded-[32px] py-3.5">
         Connect Wallet
       </button>
