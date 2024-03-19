c0628bd3759c3716a85cd502cfea0e24f9fe70b3
kelvinpraises <kelvinpraises@gmail.com>
2024-03-19 05:02:14 +0100

    feat: clean up layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,11 @@
 import "@/library/styles/globals.css";
 import { cn } from "@/library/utils";
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "VeedNet",
-//   description: "Your video compression network",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <Navbar />
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
+export const metadata: Metadata = {
+  title: "VeedNet",
+  description: "Your video compression network",
+};
 
 import { Inter, Outfit } from "next/font/google";
 import localFont from "next/font/local";
@@ -36,7 +18,6 @@ const outfit = Outfit({
   variable: "--font-outfit",
   display: "swap",
 });
-// const bebas = Bebas_Neue({ subsets: ['latin'], weight: ['400'], variable: '--font-bebas', display: 'swap' })
 const atyp = localFont({
   src: [
     {
